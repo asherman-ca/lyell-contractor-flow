65183a3aa96fa7db925ea558724d521daa19538d
Alex Sherman <asherman.ca@gmail.com>
2022-08-31 10:09:20 -0700

    placeholder text

--- src/pages/Account/Account.jsx
+++ src/pages/Account/Account.jsx
@@ -1,7 +1,13 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 const Account = () => {
-	return <div className='account'>Account</div>;
+	const [reports, setReports] = useState();
+
+	return (
+		<div className='account'>
+			Table of reports with important dates and forms / buttons to update
+		</div>
+	);
 };
 
 export default Account;
