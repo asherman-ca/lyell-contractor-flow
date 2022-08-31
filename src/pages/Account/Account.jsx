77fe00b67d18f1b11fd73fbac0ac920c8d2ff650
Alex Sherman <asherman.ca@gmail.com>
2022-08-31 15:46:52 -0700

    messy date addition init

--- src/pages/Account/Account.jsx
+++ src/pages/Account/Account.jsx
@@ -1,36 +1,50 @@
 import React, { useState, useEffect } from 'react';
 import { db } from '../../firebase.config';
-import { collection, getDocs } from 'firebase/firestore';
+import { collection, getDocs, doc, updateDoc } from 'firebase/firestore';
 
 const Account = () => {
 	const [loading, setLoading] = useState(true);
 	const [reports, setReports] = useState();
-	const [filteredReports, setFilteredReports] = useState();
 	const [tableFilter, setTableFilter] = useState('Active');
 
 	useEffect(() => {
-		const fetch = async () => {
-			const ref = collection(db, 'users');
-			const snap = await getDocs(ref);
-			let reports = [];
-			snap.forEach((el) => {
-				return reports.push({ data: el.data(), id: el.id });
-			});
-			setLoading(false);
-			setReports(reports);
-			setFilteredReports(
-				reports.filter((report) => report.data.status == tableFilter)
-			);
-		};
-
-		fetch();
-	}, []);
+		if (loading) {
+			const fetch = async () => {
+				const ref = collection(db, 'users');
+				const snap = await getDocs(ref);
+				let reports = [];
+				snap.forEach((el) => {
+					return reports.push({ data: el.data(), id: el.id });
+				});
+				setLoading(false);
+				setReports(reports);
+			};
+			fetch();
+		}
+	}, [loading]);
 
 	const handleFilter = (filter) => {
 		setTableFilter(filter);
-		setFilteredReports(
-			reports.filter((report) => report.data.status == filter)
-		);
+	};
+
+	const handleDateChange = async (id, delta, endDate) => {
+		let date = endDate.toDate();
+		date.setDate(date.getDate() + delta);
+		const userRef = doc(db, 'users', id);
+		await updateDoc(userRef, {
+			endDate: date,
+		});
+		setLoading(true);
+		// setReports((prev) => {
+		// 	return prev.map((report) => {
+		// 		if (report.id == id) {
+		// 			report.data.endDate = endDate.seconds + 86400 * delta;
+		// 			return report;
+		// 		} else {
+		// 			return report;
+		// 		}
+		// 	});
+		// });
 	};
 
 	if (loading) {
@@ -70,26 +84,52 @@ const Account = () => {
 					<div className='col'>Extend 90</div>
 				</div>
 				<div className='table-rows'>
-					{filteredReports.map((report) => {
-						return (
-							<div className='table-row'>
-								<div className='col'>{report.data.name}</div>
-								<div className='col'>{report.data.managerEmail}</div>
-								<div className='col'>
-									{report.data.endDate.toDate().toDateString()}
+					{reports
+						.filter((report) => report.data.status == tableFilter)
+						.map((report) => {
+							{
+								console.log('report', report);
+							}
+							return (
+								<div className='table-row'>
+									<div className='col'>{report.data.name}</div>
+									<div className='col'>{report.data.managerEmail}</div>
+									<div className='col'>
+										{report.data.endDate.toDate().toDateString()}
+									</div>
+									<di className='col'>
+										<div
+											className='button'
+											onClick={() =>
+												handleDateChange(report.id, 30, report.data.endDate)
+											}
+										>
+											Add Days
+										</div>
+									</di>
+									<div className='col'>
+										<div
+											className='button'
+											onClick={() =>
+												handleDateChange(report.id, 60, report.data.endDate)
+											}
+										>
+											Add Days
+										</div>
+									</div>
+									<div className='col'>
+										<div
+											className='button'
+											onClick={() =>
+												handleDateChange(report.id, 90, report.data.endDate)
+											}
+										>
+											Add Days
+										</div>
+									</div>
 								</div>
-								<di className='col'>
-									<div className='button'>Add Days</div>
-								</di>
-								<div className='col'>
-									<div className='button'>Add Days</div>
-								</div>
-								<div className='col'>
-									<div className='button'>Add Days</div>
-								</div>
-							</div>
-						);
-					})}
+							);
+						})}
 				</div>
 			</div>
 		</div>
